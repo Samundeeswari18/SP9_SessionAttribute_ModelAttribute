1e5dc2c698d018b01ae61b73a4526e1a8c875015
Samundeeswari Velusamy <sam.velusamy36@gmail.com>
2024-07-06 15:21:58 -0400

    Session Attribute and modelAttribute

---
+++ src/main/java/com/sam/sp9_sessionattribute_modelattribute/Sp9SessionAttributeModelAttributeApplication.java
@@ -0,0 +1,13 @@
+package com.sam.sp9_sessionattribute_modelattribute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp9SessionAttributeModelAttributeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sp9SessionAttributeModelAttributeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sam/sp9_sessionattribute_modelattribute/Sp9SessionAttributeModelAttributeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.sp9_sessionattribute_modelattribute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp9SessionAttributeModelAttributeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
